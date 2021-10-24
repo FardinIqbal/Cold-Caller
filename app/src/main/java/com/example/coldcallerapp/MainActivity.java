e8f8973767eb7417178882b3bad19fbbcad9f077
nomskiii <noamcanter@gmail.com>
2021-10-24 17:45:43 -0400

    Added all of the names

--- app/src/main/java/com/example/coldcallerapp/MainActivity.java
+++ app/src/main/java/com/example/coldcallerapp/MainActivity.java
@@ -70,6 +70,33 @@ public class MainActivity extends AppCompatActivity {
         mUncalledLog.add(new Student(R.string.aamir_ali, R.drawable.aamir_ali));
         mUncalledLog.add(new Student(R.string.alex_aney, R.drawable.alex_aney));
         mUncalledLog.add(new Student(R.string.bipra_dey, R.drawable.bipra_dey));
+        mUncalledLog.add(new Student(R.string.daniel_dultsin, R.drawable.daniel_dultsin));
+        mUncalledLog.add(new Student(R.string.darren_dong, R.drawable.darren_dong));
+        mUncalledLog.add(new Student(R.string.dennis_wang, R.drawable.dennis_wang));
+        mUncalledLog.add(new Student(R.string.dhruv_amin, R.drawable.dhruv_amin));
+        mUncalledLog.add(new Student(R.string.eden_kodan, R.drawable.eden_kogan));
+        mUncalledLog.add(new Student(R.string.eli_bui, R.drawable.eli_bui));
+        mUncalledLog.add(new Student(R.string.elie_belkin, R.drawable.elie_belkin));
+        mUncalledLog.add(new Student(R.string.evelyn_paskhaver, R.drawable.evelyn_paskhaver));
+        mUncalledLog.add(new Student(R.string.fardin_iqbal, R.drawable.fardin_iqbal));
+        mUncalledLog.add(new Student(R.string.jerry_he, R.drawable.jerry_he));
+        mUncalledLog.add(new Student(R.string.kenny_cao, R.drawable.kenny_cao));
+        mUncalledLog.add(new Student(R.string.marc_rosenberg, R.drawable.marc_rosenberg));
+        mUncalledLog.add(new Student(R.string.matthew_chen, R.drawable.matthew_chen));
+        mUncalledLog.add(new Student(R.string.michael_wu, R.drawable.michael_wu));
+        mUncalledLog.add(new Student(R.string.ming_lin, R.drawable.ming_lin));
+        mUncalledLog.add(new Student(R.string.mohammed_ihtisham, R.drawable.mohammed_ihtisham));
+        mUncalledLog.add(new Student(R.string.noam_canter, R.drawable.noam_canter));
+        mUncalledLog.add(new Student(R.string.ralf_pacia, R.drawable.ralf_pacia));
+        mUncalledLog.add(new Student(R.string.samuel_iskhakov, R.drawable.samuel_iskhakov));
+        mUncalledLog.add(new Student(R.string.sean_kerrigan, R.drawable.sean_kerrigan));
+        mUncalledLog.add(new Student(R.string.sebastian_marinescu, R.drawable.sebastian_marinescu));
+        mUncalledLog.add(new Student(R.string.selina_li, R.drawable.selina_li));
+        mUncalledLog.add(new Student(R.string.shuyue_chen, R.drawable.shuyue_chen));
+        mUncalledLog.add(new Student(R.string.tanushri_sundaram, R.drawable.tanushri_sundaram));
+        mUncalledLog.add(new Student(R.string.vasu_patel, R.drawable.vasu_patel));
+        mUncalledLog.add(new Student(R.string.xinrui_ge, R.drawable.xinrui_ge));
+        mUncalledLog.add(new Student(R.string.zhian_maysoon, R.drawable.zhian_maysoon));
     }
 
     public void displayStudent() {
