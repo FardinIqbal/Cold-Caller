f0172effd48fb650729c9b99c5874e721364f061
FardinIqbal <52869603+FardinIqbal@users.noreply.github.com>
2021-10-24 16:47:37 -0400

    Go back and forth between Activities

--- app/src/main/java/com/example/coldcallerapp/CalledLogActivity.java
+++ app/src/main/java/com/example/coldcallerapp/CalledLogActivity.java
@@ -2,13 +2,28 @@ package com.example.coldcallerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalledLogActivity extends AppCompatActivity {
+
+    private Button mMainMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_called_log);
+
+        mMainMenuButton = findViewById(R.id.main_menu_button);
+        mMainMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CalledLogActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
