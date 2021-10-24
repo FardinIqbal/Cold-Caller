5a139a28cf07ab3e2101cdd60e378f7e6f39f503
FardinIqbal <52869603+FardinIqbal@users.noreply.github.com>
2021-10-24 13:14:54 -0400

    CallRandom Button Works

--- app/src/main/java/com/example/coldcallerapp/MainActivity.java
+++ app/src/main/java/com/example/coldcallerapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,19 +21,44 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<Student> mStudents = new ArrayList<Student>();
     public Student currentStudent;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createStudentList();
+
+        updateCurrentStudent();
+        displayStudent();
+        displayTime();
+
+        final Button callRandom = findViewById(R.id.buttonRandom);
+        callRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateCurrentStudent();
+                updateCurrentStudent();
+                displayStudent();
+                displayTime();
+            }
+        });
+    }
+
+    public void createStudentList() {
         mStudents.add(new Student(R.string.aamir_ali, R.drawable.aamir_ali));
         mStudents.add(new Student(R.string.alex_aney, R.drawable.alex_aney));
         mStudents.add(new Student(R.string.bipra_dey, R.drawable.bipra_dey));
+    }
 
+    public void displayStudent() {
         ImageView img = (ImageView) findViewById(R.id.studentImage);
-        img.setImageResource(R.drawable.aamir_ali);
+        img.setImageResource(currentStudent.getImage());
 
+        TextView name = (TextView) findViewById(R.id.nameTextView);
+        name.setText(currentStudent.getName());
+    }
+
+    public void displayTime() {
         Date date = Calendar.getInstance().getTime();
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm");
         String strDate = dateFormat.format(date);
@@ -40,10 +67,7 @@ public class MainActivity extends AppCompatActivity {
         timeTextView.setText(strDate);
     }
 
-    public void updateCurrentStudent(){
-        currentStudent = mStudents.get((int) Math.random() * mStudents.size());
+    public void updateCurrentStudent() {
+        currentStudent = mStudents.get((int)(Math.random() * mStudents.size()));
     }
-
-
-
 }
